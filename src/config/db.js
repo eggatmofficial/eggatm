df712b76a161cc01d289a3a8ff273436781d951f
Ajithkumar Dharmalingam <ajithkumarbe97@gmail.com>
2026-01-22 07:57:36 +0530

    commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log("📡 Connecting to MongoDB...");
+  console.log(" Connecting to MongoDB...");
     console.log("db",process.env.MONGO_URI),
   await mongoose.connect(process.env.MONGO_URI, {
 
