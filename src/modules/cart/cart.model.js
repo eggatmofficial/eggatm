b07c5c3000d3e4d23ff718c3dd95c5dc0446cab6
Ajithkumar Dharmalingam <ajithkumarbe97@gmail.com>
2026-02-04 08:32:06 +0530

    cart-model change

--- src/modules/cart/cart.model.js
+++ src/modules/cart/cart.model.js
@@ -1,3 +1,55 @@
+// const mongoose = require("mongoose");
+
+// const cartItemSchema = new mongoose.Schema(
+//   {
+//     productId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Product",
+//       required: true,
+//     },
+
+//     variantLabel: {
+//       type: String, // "250g"
+//       required: true,
+//     },
+
+//     price: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+
+//     quantity: {
+//       type: Number,
+//       required: true,
+//       min: 1,
+//     },
+//       weight:{
+//       type: Number,
+//       required:true
+//     }
+//   },
+//   { _id: false }
+// );
+
+// const cartSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       unique: true,
+//       required: true,
+//     },
+
+//     items: [cartItemSchema],
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Cart", cartSchema);
+
+
+
 const mongoose = require("mongoose");
 
 const cartItemSchema = new mongoose.Schema(
@@ -24,7 +76,7 @@ const cartItemSchema = new mongoose.Schema(
       required: true,
       min: 1,
     },
-      weight:{
+    weight:{
       type: Number,
       required:true
     }
